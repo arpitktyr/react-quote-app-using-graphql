790a680fb5c8abb00b8cf15a91f1e4dd79df7010
arpitktyr <arpitktyr@gmail.com>
2023-06-17 23:26:13 +0530

    icon added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,9 +13,21 @@ export default function Header() {
   return (
     <nav>
       <div className="nav-wrapper deep-purple">
-        <Link to="/" className="brand-logo left">
+        <Link to="/" className="brand-logo left hide-on-med-and-down">
           Quote App
         </Link>
+        <Link
+          to="/"
+          className="brand-logo mob-icon left hide-on-large-only show-on-small"
+        >
+          <img
+            className="logo"
+            src="./quote.png"
+            alt="Quote Icon"
+            width={100}
+          />
+        </Link>
+
         <ul id="nav-mobile" className="right">
           <li>
             <Link to="/"> Home </Link>
