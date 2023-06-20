53233fb07913e7146e06e6ada60dd8efea75446b
arpitktyr <arpitktyr@gmail.com>
2023-06-20 18:48:36 +0530

    updated constant file

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,1 +1,5 @@
-export const GRAPHQL_API = "https://graphql-api-server.onrender.com";
+//deployed on render - bad in loading
+//export const GRAPHQL_API = "https://graphql-api-server.onrender.com";
+
+//deployed on cyclic - good in loading
+export const GRAPHQL_API = "https://cloudy-cyan-lamb.cyclic.app";
